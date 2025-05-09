db5aa8f59efe7f23c710b3486140721bde31df6d
ApueXI <apue2434@gmail.com>
2025-05-09 21:05:04 +0800

    Changed, Almost everything

--- website/models.py
+++ website/models.py
@@ -4,11 +4,11 @@ from datetime import datetime
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(150), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    stock = db.Column(db.Interger, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
     manufacturer = db.Column(db.String(30), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    date_updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    # date_updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __repr__(self):
-        return f'<Product {self.product_name}>'
+    # def __repr__(self):
+    #     return f'<Product {self.product_name}>'
--- website/views.py
+++ website/views.py
@@ -1,9 +1,27 @@
-from flask import Blueprint, render_template, url_for
-
+from flask import Blueprint, request, redirect, render_template, url_for
+from website import db
+from .models import Product
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['POST', 'GET'])
+@views.route('/', methods=['GET', 'POST'])
 def home():
-    
-    return render_template("Inventory.html")
+    if request.method == 'POST': 
+        product_name = request.form['product_name']
+        price = request.form['price']
+        stock = request.form['stock']
+        manufacturer = request.form['manufacturer']
+
+        new_product = Product(product_name=product_name, price=price, stock=stock, manufacturer=manufacturer)
+        
+        try:
+            db.session.add(new_product)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your product"
+
+    else:
+        products = Product.query.order_by(Product.date_created).all()
+        return render_template("Inventory.html", products=products)
+
