39cc60768db91692b30247ed20d749e2b669b19e
ApueXI <apue2434@gmail.com>
2025-05-09 16:55:46 +0800

    Folder changted, move files

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def run_app():
+    return render_template('Inventory.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
