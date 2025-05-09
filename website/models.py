4e534ffe7c7a1a09f90ac683278cf544fa6f0be1
ApueXI <apue2434@gmail.com>
2025-05-09 21:04:53 +0800

    Changed, alsmot everything

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from website import db
+from datetime import datetime
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,3 +7,8 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Interger, nullable=False)
     manufacturer = db.Column(db.String(30), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Product {self.product_name}>'
--- website/views.py
+++ website/views.py
@@ -3,8 +3,7 @@ from flask import Blueprint, render_template, url_for
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['POST', 'GET'])
 def home():
-
+    
     return render_template("Inventory.html")
