7ff2178ac888a7b2bd576a2fde9eb40f2eef556e
ApueXI <apue2434@gmail.com>
2025-05-09 21:43:31 +0800

    Changed files, Changed Database getting data

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, request, render_template, url_for
+from website import db
+from website.models import Product
 
 
 views = Blueprint('views', __name__)
@@ -8,21 +10,21 @@ views = Blueprint('views', __name__)
 def home():
     
         if request.method == 'POST': 
-        product_name = request.form['product_name']
-        price = request.form['price']
-        stock = request.form['stock']
-        manufacturer = request.form['manufacturer']
+            product_name = request.form['product_name']
+            price = request.form['price']
+            stock = request.form['stock']
+            manufacturer = request.form['manufacturer']
 
-        new_product = Product(product_name=product_name, price=price, stock=stock, manufacturer=manufacturer)
-        
-        try:
-            db.session.add(new_product)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue adding your product"
+            new_product = Product(product_name=product_name, price=price, stock=stock, manufacturer=manufacturer)
+            
+            try:
+                db.session.add(new_product)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return "There was an issue adding your product"
 
-    else:
-        products = Product.query.order_by(Product.date_created).all()
-        return render_template("Inventory.html", products=products)
+        else:
+            products = Product.query.order_by(Product.date_created).all()
+            return render_template("Inventory.html", products=products)
 
