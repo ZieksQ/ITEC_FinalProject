ee798745b2c2aa881905803eb72a599d7e1ce88a
Andy Sarne <sarneandy6@gmail.com>
2025-05-10 18:16:17 +0800

    organize folder and base file

--- website/static/jscript/inventory-function.js
+++ website/static/jscript/inventory-function.js
@@ -1,27 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const sideBarBtn = document.getElementById('side-bar-button');
-  const sideBar = document.getElementById('side-bar');
-  const mainContent = document.getElementById('main-content');
-
-  sideBarBtn.addEventListener('click', sideBarInteract);
-  let isVisible = false;
-  function sideBarInteract() {
-    
-
-    if (isVisible === true) {
-      sideBarBtn.style.transform = 'rotate(0deg)';
-      sideBar.style.transform = 'translateX(-250px)'; 
-      mainContent.style.transform = 'translateX(0)';
-      isVisible = false;     
-    }
-    else if (isVisible === false) {
-      sideBarBtn.style.transform = 'rotate(180deg)';
-      sideBar.style.transform = 'translateX(0)';
-      mainContent.style.transform = 'translateX(250px)';
-      isVisible = true;
-    }
-  }
-});
 
 // Get DOM elements
 const addItemButton = document.getElementById('add-item');
