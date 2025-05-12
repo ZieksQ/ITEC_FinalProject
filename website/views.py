130cb9154c7168b9e12380891177a6ac8c5765c5
ApueXI <apue2434@gmail.com>
2025-05-12 10:55:37 +0800

    price format

--- website/views.py
+++ website/views.py
@@ -86,11 +86,15 @@ def search():
     else:
         flash('No product found!', category='error')
         searches = Product.query.all()
+
+    for product in searches:
+        product.price = format_price(product.price)
         
     return render_template('search.html', searches=searches)
 
 @views.route('/inventory_sorted_by_the_stock', methods=['POST', 'GET'])
-def sorted_by():
+def sorted_by_inventory():
+
     sort = request.args.get('sort', 'prodcut_name')
     order = request.args.get('order', 'asc')
 
@@ -109,7 +113,36 @@ def sorted_by():
         products = Product.query.order_by(columnstock.asc()).all()
         products = Product.query.order_by(columnid.asc()).all()
 
+    for product in products:
+            product.price = format_price(product.price)
+
     return render_template('Inventory.html', products=products, order=order, sort=sort)
+
+@views.route('/search_sorted_by_the_stock', methods=['POST', 'GET'])
+def sorted_by_search():
+
+    sort = request.args.get('sort', 'prodcut_name')
+    order = request.args.get('order', 'asc')
+
+    columnstock = getattr(Product, sort, Product.stock)
+    columnid = getattr(Product, sort, Product.id)
+    
+    if order == 'desc_stock':
+        searches = Product.query.order_by(columnstock.desc()).all()
+    elif order == 'asc_stock':
+        searches = Product.query.order_by(columnstock.asc()).all()
+    elif order == 'desc_price':
+        searches = Product.query.order_by(columnid.desc()).all()
+    elif order == 'asc_price':
+        searches = Product.query.order_by(columnid.asc()).all()
+    else:
+        searches = Product.query.order_by(columnstock.asc()).all()
+        searches = Product.query.order_by(columnid.asc()).all()
+
+    for product in searches:
+            product.price = format_price(product.price)
+
+    return render_template('search.html', searches=searches, order=order, sort=sort)
 
 def format_price(price):
     return f"₱{float(price):,.2f}"
