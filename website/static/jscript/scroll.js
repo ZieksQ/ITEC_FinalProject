9f66efedf48952a0c141e28c92f4332998390c83
ZieksQ <nexisonytb@gmail.com>
2025-05-16 01:59:18 +0800

    added scrollreveal

---
+++ website/static/jscript/scroll.js
@@ -0,0 +1,39 @@
+// ScrollReveal().reveal('.section-1', { easing: 'ease-in', interval: 500});
+// ScrollReveal().reveal(".scroll-animation", {delay: 200, easing: 'ease-in', interval: 200, scale: 0.8});
+
+//next feature reset: true
+ScrollReveal().reveal("#scroll-interval", {
+    delay: 200, 
+    interval: 400
+})
+
+ScrollReveal().reveal(".section-2", {
+    distance: '50px',
+    origin: 'bottom',
+    duration: '400',
+    easing: 'ease-in'
+})
+
+ScrollReveal().reveal(".feature-block", {
+    interval: 500,
+    distance: '50px',
+    origin: 'bottom',
+    duration: '600',
+    easing: 'ease-in'
+})
+
+ScrollReveal().reveal(".section-4", {
+    distance: '50px',
+    origin: 'bottom',
+    duration: '600',
+    easing: 'ease-in',
+    viewFactor: 0.7
+})
+
+ScrollReveal().reveal(".footer-animation", {
+    distance: '50px',
+    origin: 'bottom',
+    duration: '500',
+    easing: 'ease-in',
+    viewFactor: 0.5
+})
