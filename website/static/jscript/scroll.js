342e1eef083414828f6c211483a3cc9998efb3de
pasteUsername <bsit.snowden.1999@gmail.com>
2025-05-16 10:53:55 +0800

    modify code for scroll animation

--- website/static/jscript/scroll.js
+++ website/static/jscript/scroll.js
@@ -2,38 +2,47 @@
 // ScrollReveal().reveal(".scroll-animation", {delay: 200, easing: 'ease-in', interval: 200, scale: 0.8});
 
 //next feature reset: true
-ScrollReveal().reveal("#scroll-interval", {
-    delay: 200, 
-    interval: 400
+ScrollReveal({
+    delay: 200,
+    interval: 400,
+    reset: true
 })
 
-ScrollReveal().reveal(".section-2", {
+// ScrollReveal().reveal("#scroll-interval", {
+//     delay: 200, 
+//     interval: 400
+// })
+
+// ScrollReveal().reveal(".section-1", {distance: '50px', origin: 'bottom', delay: 500})
+
+ScrollReveal().reveal(".section-1, .section-2, .section-4, .footer-animation", {
+    interval: 500,
     distance: '50px',
     origin: 'bottom',
     duration: '400',
     easing: 'ease-in'
 })
 
-ScrollReveal().reveal(".feature-block", {
-    interval: 500,
-    distance: '50px',
-    origin: 'bottom',
-    duration: '600',
-    easing: 'ease-in'
-})
+ScrollReveal().reveal(".feature-block", {interval: 300, distance: '50px', origin: 'bottom', duration: '300', easing: 'ease-in'})
 
-ScrollReveal().reveal(".section-4", {
-    distance: '50px',
-    origin: 'bottom',
-    duration: '600',
-    easing: 'ease-in',
-    viewFactor: 0.7
-})
+// ScrollReveal().reveal(".feature-block", {
+//     interval: 500,
+//     distance: '50px',
+//     origin: 'bottom',
+//     duration: '400',
+//     easing: 'ease-in'
+// })
 
-ScrollReveal().reveal(".footer-animation", {
-    distance: '50px',
-    origin: 'bottom',
-    duration: '500',
-    easing: 'ease-in',
-    viewFactor: 0.5
-})
+// ScrollReveal().reveal(".section-4", {
+//     distance: '50px',
+//     origin: 'bottom',
+//     duration: '400',
+//     easing: 'ease-in',
+// })
+
+// ScrollReveal().reveal(".footer-animation", {
+//     distance: '50px',
+//     origin: 'bottom',
+//     duration: '400',
+//     easing: 'ease-in',
+// })
