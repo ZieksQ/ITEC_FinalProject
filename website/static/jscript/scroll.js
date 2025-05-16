a528a87eb269dafd247b305221979c18d7ace89c
ZieksQ <nexisonytb@gmail.com>
2025-05-17 01:51:36 +0800

    modified animation, search, etc.

--- website/static/jscript/scroll.js
+++ website/static/jscript/scroll.js
@@ -1,19 +1,8 @@
-// ScrollReveal().reveal('.section-1', { easing: 'ease-in', interval: 500});
-// ScrollReveal().reveal(".scroll-animation", {delay: 200, easing: 'ease-in', interval: 200, scale: 0.8});
-
-//next feature reset: true
 ScrollReveal({
     delay: 200,
     interval: 400,
     reset: true
-})
-
-// ScrollReveal().reveal("#scroll-interval", {
-//     delay: 200, 
-//     interval: 400
-// })
-
-// ScrollReveal().reveal(".section-1", {distance: '50px', origin: 'bottom', delay: 500})
+});
 
 ScrollReveal().reveal(".section-1, .section-2, .section-4, .footer-animation", {
     interval: 500,
@@ -21,28 +10,8 @@ ScrollReveal().reveal(".section-1, .section-2, .section-4, .footer-animation", {
     origin: 'bottom',
     duration: '400',
     easing: 'ease-in'
-})
+});
 
-ScrollReveal().reveal(".feature-block", {interval: 300, distance: '50px', origin: 'bottom', duration: '300', easing: 'ease-in'})
+ScrollReveal().reveal(".feature-block", {interval: 300, distance: '50px', origin: 'bottom', duration: '300', easing: 'ease-in'});
 
-// ScrollReveal().reveal(".feature-block", {
-//     interval: 500,
-//     distance: '50px',
-//     origin: 'bottom',
-//     duration: '400',
-//     easing: 'ease-in'
-// })
-
-// ScrollReveal().reveal(".section-4", {
-//     distance: '50px',
-//     origin: 'bottom',
-//     duration: '400',
-//     easing: 'ease-in',
-// })
-
-// ScrollReveal().reveal(".footer-animation", {
-//     distance: '50px',
-//     origin: 'bottom',
-//     duration: '400',
-//     easing: 'ease-in',
-// })
+ScrollReveal().reveal(".sec-4-text", {distance: '50px', origin: 'left', duration: '400', easing: 'ease-in'});
